94550d3e23b45b67bcf33659200c38cceeca12bf
CIANDT\guilhermecf <guilhermecf@ciandt.com>
2022-04-21 19:58:20 -0300

    Entity for Currency created

---
+++ src/main/java/br/com/crypto/entity/Currency.java
@@ -0,0 +1,32 @@
+package br.com.crypto.entity;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "currency")
+public class Currency {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    private String name;
+
+    private String code;
+
+    private String createdAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+}
+
+//    name VARCHAR(45),
+//    code VARCHAR(45),
+//    createdAt TIMESTAMP
