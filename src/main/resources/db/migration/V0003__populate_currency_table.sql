1434e81bb80b85a8237fe57379abaf06340ac1c4
CIANDT\guilhermecf <guilhermecf@ciandt.com>
2022-04-21 20:29:55 -0300

    Added new migrations to change currency table and add new mocked data.

---
+++ src/main/resources/db/migration/V0003__populate_currency_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.currency (id, "name", code, createdat) VALUES('00f6722a-2f0d-4267-b04a-cffd20e0d22a'::uuid, 'Bitcoin', 'BTC', NULL);
+INSERT INTO public.currency (id, "name", code, createdat) VALUES('ee157b06-9412-4353-8033-a6daf535a183'::uuid, 'Etherium', 'ETH', NULL);
+INSERT INTO public.currency (id, "name", code, createdat) VALUES('a00e589d-8fe8-4c07-a614-96b9d8fe8202'::uuid, 'Tether', 'USDT', NULL);
