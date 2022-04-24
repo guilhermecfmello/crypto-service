359962092420689bd969c60ec41b55d5995c6aee
CIANDT\guilhermecf <guilhermecf@ciandt.com>
2022-04-24 18:21:36 -0300

    Added setter for createdat attr in Currency class.

--- src/main/java/br/com/crypto/entity/Currency.java
+++ src/main/java/br/com/crypto/entity/Currency.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 public class Currency {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
+    @Column(name = "id", nullable = false, insertable = false, updatable = false)
     private UUID id;
 
     private String name;
@@ -17,6 +17,10 @@ public class Currency {
 
     @Column(name="createdat", insertable = false, updatable = false)
     private String createdAt;
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
 
     public Currency(String name, String code) {
         this.name = name;
