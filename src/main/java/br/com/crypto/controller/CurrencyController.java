76bae579fe18166f3da03e534f06d3bb0ce687ce
CIANDT\guilhermecf <guilhermecf@ciandt.com>
2022-04-24 18:22:40 -0300

    Updated post and put method in Currency Controller, using DTO instead of entity for Currency classes.

--- src/main/java/br/com/crypto/controller/CurrencyController.java
+++ src/main/java/br/com/crypto/controller/CurrencyController.java
@@ -1,5 +1,6 @@
 package br.com.crypto.controller;
 
+import br.com.crypto.dto.CurrencyDTO;
 import br.com.crypto.entity.Currency;
 import br.com.crypto.exception.CurrencyNotFoundException;
 import br.com.crypto.repository.CurrencyRepository;
@@ -28,8 +29,8 @@ public class CurrencyController {
     // end::get-aggregate-root[]
 
     @PostMapping("/currencies")
-    Currency newCurrency(@RequestBody Currency newCurrency) {
-        return repository.save(newCurrency);
+    Currency newCurrency(@RequestBody CurrencyDTO newCurrency) {
+        return repository.save(newCurrency.toEntity());
     }
 
     // Single item
@@ -46,19 +47,15 @@ public class CurrencyController {
         repository.deleteById(id);
     }
 
+    //TODO validate json input in case that name or code is null
+    // maybe its done through model class
     @PutMapping("/currencies/{id}")
     Currency replaceCurrency(@RequestBody Currency newCurrency, @PathVariable UUID id){
-        return repository.findById(id)
-            .map(
-                currency -> {
-                    currency.setName(newCurrency.getName());
-                    currency.setCode(newCurrency.getCode());
-                    return repository.save(currency);
-                })
-                .orElseGet(() -> {
-                    newCurrency.setId(id);
-                    return repository.save(newCurrency);
-                });
+        Currency currencyEntity = repository.findById(id)
+                .orElseThrow(() -> new CurrencyNotFoundException(id));
+        CurrencyDTO currencyDTO = new CurrencyDTO(newCurrency);
+
+        return repository.save(currencyDTO.toEntity());
     }
 
 }
