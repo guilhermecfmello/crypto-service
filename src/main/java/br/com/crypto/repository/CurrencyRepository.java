2962fbaff4ca8c98f395b38ad1e65bbdbe955b99
CIANDT\guilhermecf <guilhermecf@ciandt.com>
2022-04-25 11:52:11 -0300

    Fix Entity import

--- src/main/java/br/com/crypto/repository/CurrencyRepository.java
+++ src/main/java/br/com/crypto/repository/CurrencyRepository.java
@@ -1,6 +1,6 @@
 package br.com.crypto.repository;
 
-import br.com.crypto.entity.Currency;
+import br.com.crypto.repository.entity.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
